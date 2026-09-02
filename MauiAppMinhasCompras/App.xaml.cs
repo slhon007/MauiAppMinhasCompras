15d2226ad6bb13e57cd6cf62824029b1b25e03bf
Gabriel Bittencourt <gabrielcamposbittencourt@hotmail.com>
2026-09-02 10:52:30 -0300

    correção de erro na troca de tela inicial

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -7,13 +7,11 @@ namespace MauiAppMinhasCompras
         public App()
         {
             InitializeComponent();
-
-            MainPage = new NavigationPage(new Views.ListaProduto());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            return new Window(new Views.ListaProduto());
         }
     }
 }
