753e156e5baf406b25e3ed92805495e4afc301fd
Gabriel Bittencourt <gabrielcamposbittencourt@hotmail.com>
2026-09-02 15:00:35 -0300

    colocando o CRUD no mysqlite

--- MauiAppMinhasCompras/Helpers/SQliteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQliteDatabaseHelper.cs
@@ -1,10 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SQLite;
+using MauiAppMinhasCompras.Models;
+
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    internal class SQliteDatabaseHelper
+    public class SQliteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabseHelper(string path) 
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p) 
+        {
+            return _conn.InsertAsync(p);
+        }
+        public Task<List<Produto>> Update(Produto p) 
+        {
+            string sql = "UPDATE Produto SET Descrição = ?, Quantidade = ?, Preco = ? WHERE Id = ?";
+
+            return _conn.QueryAsync<Produto>(
+                sql, p.Descricao, p. Quantidade,p.Preco, p.Id
+                );
+        }
+
+        public Task<int> Delete(int id) 
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => id == id); 
+        }
+
+        public Task<List<Produto>> GetAll() 
+        { 
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<int> Search(string q) 
+        {
+            string sql = "SELECT * Produto SET  WHERE descricao LIKE '%"+q+"%";
+
+            return _conn.QueryAsync<Produto>(sql);
+        }
+
     }
 }
